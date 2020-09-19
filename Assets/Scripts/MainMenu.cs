9534395c7286a64f43f2bb46c32cc6e0bce231ee
BLUE110601 <57922440+BLUE110601@users.noreply.github.com>
2020-09-18 18:54:21 -0600

    got some progress with artifacts and also made a player movement script that works with WASD and arrow keys

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void playGame()
+   public void playGame()
     {
-        Debug.log("Hello");
+        SceneManager.LoadScene(1);
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.IO;
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int horizontalSpeed;
+    public int verticalSpeed;
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * horizontalSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * horizontalSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * verticalSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * verticalSpeed * Time.deltaTime;
+        }
+
+    }
+    
+}
